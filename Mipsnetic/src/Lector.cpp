e058913d6ec01e4187c6af1150e09ca5da0f64d8
Ricardo <fankashai@gmail.com>
2016-05-23 19:30:24 -0600

    Creacion del proyecto

---
+++ Mipsnetic/src/Lector.cpp
@@ -0,0 +1,29 @@
+/*
+ * Lector.cpp
+ *
+ *  Created on: May 21, 2016
+ *      Author: ricardo
+ */
+
+#include "Lector.h"
+
+Lector::Lector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+string Lector:: parsear(string path){
+	ifstream ifs("/home/ricardo/Documents/Assembla/binarioMetodoData");
+	//ifstream ifs(path);
+	string content( (istreambuf_iterator<char>(ifs) ),
+	                         (istreambuf_iterator<char>()    ) );
+
+	cout<<content<<endl;
+
+	return content;
+}
+
+Lector::~Lector() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Mipsnetic/src/Main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: May 21, 2016
+ *      Author: ricardo
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Lector.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	Lector l;
+	l.parsear("af");
+    return 0;
+}
+
