4357e7037e71692ef113df5f8a225b8fe66f5362
kashai55 <fankashai@hotmail.com>
2016-05-25 15:15:06 -0600

    Cambio en Lector.cpp

--- Mipsnetic/src/Lector.cpp
+++ Mipsnetic/src/Lector.cpp
@@ -12,18 +12,35 @@ Lector::Lector() {
 
 }
 
-string Lector:: parsear(string path){
-	ifstream ifs("/home/ricardo/Documents/Assembla/binarioMetodoData");
+void Lector:: parsear(string path){
+	ifstream ifs("/home/ricardo/Documents/Assembla/congruencialBinarioBinarioText");
 	//ifstream ifs(path);
 	string content( (istreambuf_iterator<char>(ifs) ),
 	                         (istreambuf_iterator<char>()    ) );
 
-	cout<<content<<endl;
+	this->textoBin=content;
+	this->cantLineas=((content.length()-1)/32);
 
-	return content;
+	//cout<< textoBin <<endl;
+	return;
+}
+
+LinkedList Lector:: dividirArchivo(){
+
+	string texto=this->textoBin;
+	int cantDigitos=this->textoBin.length();
+
+	LinkedList lista;
+	int i=0;
+	while(i < this->cantLineas){
+		lista.addNode(texto.substr(0,32));
+		texto=texto.substr(33,cantDigitos);
+		//cout<<lista[i]<<endl;
+		i=i+1;
+	}
+	return lista;
 }
 
 Lector::~Lector() {
 	// TODO Auto-generated destructor stub
 }
-
