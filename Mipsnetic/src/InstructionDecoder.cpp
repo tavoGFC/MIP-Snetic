c4de0b17ee8349723ddffae13f992a07aef59179
tavoGFC <gusfcarrera@hotmail.es>
2016-05-25 17:45:36 -0600

    Update InstructionDecoder.cpp
    Metodo para identificar los registros

--- Mipsnetic/src/InstructionDecoder.cpp
+++ Mipsnetic/src/InstructionDecoder.cpp
@@ -91,6 +91,116 @@ string InstructionDecoder :: decode_J(string binaryCode){
 		}
 }
 
+string InstructionDecoder::docodeRegister(string binaryRegisters){
+	int registerD = conversor.convertirBinario(binaryRegisters);
+
+	if( registerD == 0){
+		cout << "$zero" << endl;
+			return "$zero";
+		}
+	if( registerD == 1){
+		cout << "$at" << endl;
+			return "$at";
+		}
+	if( registerD == 2){
+		cout << "$v0" << endl;
+			return "$v0";
+		}
+	if( registerD == 3){
+		cout << "$v1" << endl;
+			return "$v1";
+		}
+	if( registerD == 4){
+		cout << "$a0" << endl;
+			return "$a0";
+		}
+	if( registerD == 5){
+		cout << "$a1" << endl;
+			return "$a1";
+		}
+	if( registerD == 6){
+		cout << "$a2" << endl;
+			return "$a2";
+		}
+	if( registerD == 7){
+		cout << "$a3" << endl;
+			return "$a3";
+		}
+	if( registerD == 8){
+		cout << "$t0" << endl;
+			return "$t0";
+		}
+	if( registerD == 9){
+		cout << "$t1" << endl;
+			return "$t1";
+		}
+	if( registerD == 10){
+		cout << "$t2" << endl;
+			return "$t2";
+		}
+	if( registerD == 11){
+		cout << "$t3" << endl;
+			return "$t3";
+		}
+	if( registerD == 12){
+		cout << "$t4" << endl;
+			return "$t4";
+		}
+	if( registerD == 13){
+		cout << "$t5" << endl;
+			return "$t5";
+		}
+	if( registerD == 14){
+		cout << "$t6" << endl;
+			return "$t6";
+		}
+	if( registerD == 15){
+		cout << "$t7" << endl;
+			return "$t7";
+		}
+	if( registerD == 16){
+		cout << "$s0" << endl;
+		return "$s0";
+		}
+	if( registerD == 17){
+		cout << "$s1" << endl;
+		return "$s1";
+		}
+	if( registerD == 18){
+		cout << "$s2" << endl;
+		return "$s2";
+		}
+	if( registerD == 19){
+		cout << "$s3" << endl;
+		return "$s3";
+		}
+	if( registerD == 20){
+		cout << "$s4" << endl;
+		return "$s4";
+		}
+	if( registerD == 21){
+		cout << "$s5" << endl;
+		return "$s5";
+		}
+	if( registerD == 22){
+		cout << "$s6" << endl;
+		return "$s6";
+		}
+	if( registerD == 23){
+		cout << "$s7" << endl;
+		return "$s7";
+		}
+	if( registerD == 24){
+		cout << "$t8" << endl;
+		return "$t8";
+		}
+	if( registerD == 25){
+		cout << "$t9" << endl;
+		return "$t9";
+		}
+
+}
+
 InstructionDecoder::~InstructionDecoder() {
 	// TODO Auto-generated destructor stub
 }
