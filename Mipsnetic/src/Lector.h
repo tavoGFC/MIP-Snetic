a2f0b7aa7762661145dde2385f325fe65cefbefd
kashai55 <fankashai@hotmail.com>
2016-05-25 15:15:49 -0600

    Cambio en Lector.h

--- Mipsnetic/src/Lector.h
+++ Mipsnetic/src/Lector.h
@@ -10,12 +10,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "LinkedList.h"
 using namespace std;
 
 class Lector {
+private:
+	string textoBin;
+	int cantLineas;
+
 public:
+
 	Lector();
-	string parsear(string path);
+
+	void parsear(string path); //parsea el archivo de texto, en este caso se le pasaran las rutas donde se encuentran los archivos que contienen los binarios a parsear
+	//dividirLineas();
+	LinkedList dividirArchivo(); //se utiliza despues de haber parseado, divide las lineas de los binarios y agrega las lineas a un array de lineas
+
 	virtual ~Lector();
 };
 
